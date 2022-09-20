eafd4ab2e32fbf40a77cf0b98837bfcb133620c8
IgorVernaschi <database.bionitec@gmail.com>
2022-09-20 09:17:08 -0400

    Enviando ao GitHub

---
+++ strings.php
@@ -0,0 +1,11 @@
+<?php
+
+$idade = 24;
+echo "Olá mundo!" . PHP_EOL;
+
+echo "Olá mundo! Minha idade é $idade anos" ;
+
+echo "Você só pode entrar, se tiver mais de 18 anos \n";
+
+echo "Você tem $idade anos. Pode entrar";
+
---
+++ tipos.php
@@ -0,0 +1,20 @@
+<?php
+
+//integer
+
+$idade = 24;
+$salario = 1000.30;
+
+//double
+
+$divisao = 10 / 3;
+
+//string
+
+$texto = "Olá mundo";
+
+// boolean
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($texto);
